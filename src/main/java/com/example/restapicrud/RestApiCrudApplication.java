f5296a7f7fd1332b5b0e610622e9fa77f7f67120
ygorkwan2001 <igorkwan18@gmail.com>
2022-11-04 22:47:49 -0300

    Atividade para a Disciplina de Sistemas Distribuidos Criação de um Crud e depois feita a conexão com o Banco de Dados

---
+++ src/main/java/com/example/restapicrud/RestApiCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapicrud;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RestApiCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restapicrud/RestApiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapicrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
